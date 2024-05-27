c722f2f328cd7a661f13d40122e0a00c8868cb5a
Snehil Shrivastava <snehilshrivastava3@gmail.com>
2024-05-28 00:19:47 +0530

    finishing breadcrumbs

--- app/components/Breadcrumbs.tsx
+++ app/components/Breadcrumbs.tsx
@@ -8,13 +8,13 @@ import { GoHome } from "react-icons/go";
 const Breadcrumbs = () => {
   const pathname = usePathname();
   const pathnames = pathname.split("/").filter((x) => x);
-  const title = pathnames ? pathnames.pop() : "";
+  const title = pathnames ? pathnames.slice(-1)[0] : "";
   return (
     <nav className="py-3 rounded-md w-full mb-4 mt-[50px]">
-      <span className="text-xl font-bold">
+      <span className="text-2xl font-semibold">
         {title && title[0].toUpperCase() + title.slice(1)}
       </span>
-      <ol className="list-reset flex text-grey-dark">
+      <ol className="list-reset flex text-grey-dark mt-2.5">
         <li className="text-[22px]">
           <Link href="/" className="text-blue-600 hover:text-blue-700">
             <GoHome />
