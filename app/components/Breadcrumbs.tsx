97ddcad4d5756c7f6fc6a2c0d11e5f9d4e6f599a
Snehil Shrivastava <snehilshrivastava3@gmail.com>
2024-05-27 23:29:15 +0530

    styling breadcrumbs

--- app/components/Breadcrumbs.tsx
+++ app/components/Breadcrumbs.tsx
@@ -9,9 +9,9 @@ const Breadcrumbs = () => {
   const pathname = usePathname();
   const pathnames = pathname.split("/").filter((x) => x);
   return (
-    <nav className="bg-gray-100 py-3 px-5 rounded-md w-full mb-4">
+    <nav className="py-3 rounded-md w-full mb-4 mt-[50px]">
       <ol className="list-reset flex text-grey-dark">
-        <li>
+        <li className="text-[22px]">
           <Link href="/" className="text-blue-600 hover:text-blue-700">
             <GoHome />
           </Link>
@@ -21,12 +21,12 @@ const Breadcrumbs = () => {
           const isLast = index === pathnames.length - 1;
 
           return isLast ? (
-            <li key={to} className="text-gray-500">
+            <li key={to} className="text-gray-500 text-[22px]">
               <span className="mx-2">/</span>
               {value}
             </li>
           ) : (
-            <li key={to}>
+            <li key={to} className="text-[22px]">
               <span className="mx-2">/</span>
               <Link href={to} className="text-blue-600 hover:text-blue-700">
                 {value}
