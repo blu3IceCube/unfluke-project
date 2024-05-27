e05342b2f04757ac4666138c93ffa1271b9b865b
Snehil Shrivastava <snehilshrivastava3@gmail.com>
2024-05-27 21:01:30 +0530

    fixing bugs

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 
-const Layout = () => {
+const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <Navbar />
+      {children}
     </>
   );
 };
