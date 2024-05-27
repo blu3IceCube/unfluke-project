17a23a77e01338e4109a2ba425ebd0f530ae5357
Snehil Shrivastava <snehilshrivastava3@gmail.com>
2024-05-27 22:44:10 +0530

    fixing breadcrumb errors

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -1,12 +1,6 @@
-"use client";
-import dynamic from "next/dynamic";
-
 import React from "react";
 import Navbar from "../components/Navbar";
-
-const Breadcrumbs = dynamic(() => import("../components/Breadcrumbs"), {
-  ssr: false,
-});
+import Breadcrumbs from "../components/Breadcrumbs";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
