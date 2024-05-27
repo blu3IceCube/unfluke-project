5a809d545d07b7fc5187f9afdbaafe0556535b36
Snehil Shrivastava <snehilshrivastava3@gmail.com>
2024-05-27 20:50:23 +0530

    fixing file structure

---
+++ app/(pages)/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Leaderboard from "./leaderboard/page";
+import Scanners from "./scanners/page";
+
+const App = () => {
+  return (
+    <>
+      <Leaderboard />
+      <Scanners />
+    </>
+  );
+};
+
+export default App;
