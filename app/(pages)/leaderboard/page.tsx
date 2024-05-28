a34301cf73864281b8332991fab587778d928022
Snehil Shrivastava <snehilshrivastava3@gmail.com>
2024-05-28 13:24:23 +0530

    styling table

--- app/(pages)/leaderboard/page.tsx
+++ app/(pages)/leaderboard/page.tsx
@@ -2,20 +2,22 @@ import React from "react";
 
 const Leaderboard = () => {
   return (
-    <div className="w-full border">
+    <div className="w-full border rounded-md p-5">
       <h1>Basic Backtest</h1>
       <div>
-        <table>
-          <tr>
-            <th>Rank</th>
-            <th>Name</th>
-            <th>Calmar Ratio</th>
-            <th>Overall Profit</th>
-            <th>Avg. Daily Profit</th>
-            <th>Win %(Day)</th>
-            <th>Price (Rs)</th>
-            <th>Action</th>
-          </tr>
+        <table className="table-auto">
+          <thead>
+            <tr>
+              <th>Rank</th>
+              <th>Name</th>
+              <th>Calmar Ratio</th>
+              <th>Overall Profit</th>
+              <th>Avg. Daily Profit</th>
+              <th>Win %(Day)</th>
+              <th>Price (Rs)</th>
+              <th>Action</th>
+            </tr>
+          </thead>
         </table>
       </div>
     </div>
