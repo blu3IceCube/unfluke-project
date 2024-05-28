2fb1ce25483a271471c3d189dc7e96eed40ce02d
Snehil Shrivastava <snehilshrivastava3@gmail.com>
2024-05-28 13:12:58 +0530

    adding table

--- app/(pages)/leaderboard/page.tsx
+++ app/(pages)/leaderboard/page.tsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const Leaderboard = () => {
-  return <div>Leaderboard</div>;
+  return (
+    <div className="">
+      <h1>Basic Backtest</h1>
+      <div>
+        <table>
+          <tr>
+            <th>Rank</th>
+            <th>Name</th>
+            <th>Calmar Ratio</th>
+            <th>Overall Profit</th>
+            <th>Avg. Daily Profit</th>
+            <th>Win %(Day)</th>
+            <th>Price (Rs)</th>
+            <th>Action</th>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Leaderboard;
